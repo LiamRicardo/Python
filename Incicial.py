b697b42beee52cbaac60df9f9bf69994c84724ce
Liam Ricardo <pubgeeuu44@gmail.com>
2024-06-24 17:22:37 -0300

    vercion mamawema

---
+++ Incicial.py
@@ -0,0 +1,40 @@
+print("Ola mundo")
+
+
+print("Ese e o Python")
+
+print("Primeiro estamos practocando o comando Print")
+
+print("Este comadno Printa 3 linhas de texto")
+
+
+print("Liam Ricardo Rodriguez legra")
+print("Centenario")
+print(18)
+#meu primeiro Script Python
+
+nome = input("Digite seu nome")
+print("Ola," + nome)
+nome = input("Digite seu nome")
+print("Ola," + nome)
+
+#string false
+resultado2 = 13
+resultado = 25
+
+
+print(f"O resultado da conta e: {resultado},e a conta de dosi valores {resultado2}" )
+
+
+nome = "Tim tester"
+idade = 18
+skill1 = "python"
+level1 = "Iniciante"
+skill2 = "java"
+level2 = "senior"
+skill3 = "C++"
+level3 = "pleno"
+baixo = 2000
+alto = 3000
+
+
