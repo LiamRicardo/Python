9412712cfae83fad03e3a790a12b325a2221e870
Liam Ricardo <pubgeeuu44@gmail.com>
2024-06-25 16:53:28 -0300

    vercion conhcaesumadre

--- Incicial.py
+++ Incicial.py
@@ -46,21 +46,82 @@ print(f"Meu nome e {nomex},tenho {idade} anos  minhas habilibdades são {skill1}
 
 #ACTIVIDADE 1
 
-nome = input("Digite seu Nome")
-print("Nome propio:," + nome)
-apelido = input("Digite seu Apelido")
-print("Apelido:," + apelido)
-endereço = input("Digite seu Endereço")
-print("Endereço:," + endereço)
-postal = input("Digite seu Codigo Postal")
-print("Ciudade e Codigo Postal:," + postal)
-rua = input("Digite seu localida oseja a Rua")
-print(f"Localidade:, {rua},Endereço {endereço},codigo postal {postal}")
+#nome = input("Digite seu Nome")
+#print("Nome propio:," + nome)
+#apelido = input("Digite seu Apelido")
+#print("Apelido:," + apelido)
+#endereço = input("Digite seu Endereço")
+#print("Endereço:," + endereço)
+#postal = input("Digite seu Codigo Postal")
+#print("Ciudade e Codigo Postal:," + postal)
+#rua = input("Digite seu localida oseja a Rua")
+#print(f"Localidade:, {rua},Endereço {endereço},codigo postal {postal}")
 
 
 
 #ACTIVIDADE 2
 
-nome1 = input("Po favor dijite seu Nome:")
-ano = input("Por favor dijite um ano:")
-print(f"{nome1} é um valente calavaira,nascida no ano de {ano}.Certa manhã,{nome1} acabou com um barulho terrivel:um dragão se aproximava da aldeia.Somente {nome1} poderia salvar os moradores da aldeia")
+
+#nome1 = input("Po favor dijite seu Nome:")
+#ano = input("Por favor dijite um ano:")
+#print(f"{nome1} é um valente calavaira,nascida no ano de {ano}.Certa manhã,{nome1} acabou com um barulho terrivel:um dragão se aproximava da aldeia.Somente {nome1} poderia salvar os moradores da aldeia")
+
+
+#ACTIVIDADE 3
+
+#cidade = "São Paulo"
+#print(f"{cidade} e Rio de Janeiro")
+
+
+#ACTIVIDADE 4
+#em1 = "Ola"
+#em2 = 24
+#int_number = 25
+
+#float_number = float(int_number)
+#print(float_number)
+
+
+
+
+
+#ACTIVIDADE 5
+#pal = "A vida e uma poja caralio me cago en todo"
+#pol = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA joder"
+#print(f"{pal} {pol}")
+
+
+
+
+
+
+
+
+
+
+#Tipos de Datos (int)Enetro,(str)Stringe,(float) .(type)
+
+#ano = int(input("Que ano voce nasceu? "))
+#print(f"Ao fim de 2024 voce tera:{2024 - ano}")
+
+#altura = float(input("Qual sua altura?"))
+#peso = float(input("Qual seu peso? "))
+
+#imc = peso / (altura / 100) ** 2
+#print(f"O IMC e: {imc}")
+
+
+
+
+#ACTIVIDADE 6
+numero = int(input("Dijite un numero:"))
+inc = numero * 5
+print(f"{numero} veses 5 e {inc}")
+
+
+
+
+#ACTIVIDADE 7
+nome = input("Que e seu nome")
+ano = int(input("Que ano voce nasceu? "))
+print(f"Ao fim de 2024 voce tera:{2024 - ano}")
